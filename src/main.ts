0f1406e26b4a7c9e376d3f5272431ddd1d7e37fd
Maryshev Andrey <a.maryshev@iqdev.digital>
2023-07-25 15:05:42 +0500

    fix url

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const url = process.env.CYCLIC_URL;
+  console.log(url);
 
   await app.listen(url || 3000);
 }
